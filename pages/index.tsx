18fa2a1699a55536b2c8ec1d9affe905b8ae3791
Sajjad <sajjad.javazi.01@gmail.com>
2024-09-01 17:40:26 +0330

    feat(index): about and contact page linked

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,12 +11,27 @@ export default function Home() {
 
       <main>
         <nav></nav>
-        <div className="bg-radial-gradient h-screen text-center text-white text-xl py-80">
+        <div className="bg-radial-gradient h-screen text-center text-white text-xl py-60">
           <h1>Sajjad Javazi</h1>
           <p>
             My name is Sajjad, I`m a Front-end developer.
           </p>
+          <div className="mt-20 bg-amber-400 p-6 rounded-full mx-40">
+          <Link href="/about">
+            <p>
+              <h3>About Me &rarr;</h3>
+              <p>Find out more about who I am and what I do.</p>
+            </p>
+          </Link>
+          <Link href="/contact">
+            <p className="mt-10">
+              <h3>Contact Me &rarr;</h3>
+              <p>Get in touch with me for collaboration or queries.</p>
+            </p>
+          </Link>
         </div>
+        </div>
+        
       </main>
     </div>
   );
