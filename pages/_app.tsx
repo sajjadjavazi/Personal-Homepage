ebcf48dc085a406bcc8b0709f080729f582b743b
sajjadjavazi <sajjad.javazi.01@gmail.com>
2024-09-08 19:05:07 +0330

    feat(_app): react-query configured

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import '../styles/globals.css'; // Your global styles if any
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// The custom App component
+function MyApp({ Component, pageProps }: AppProps) {
+  // Create a new QueryClient instance
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
